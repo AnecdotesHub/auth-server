77113d22f9ec3ae8175ede065f95b17103d8d0ce
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 16:03:54 +0200

    Add IdentityServer

---
+++ src/Jevstafjev.Anecdotes.AuthServer/Jevstafjev.Anecdotes.AuthServer.Web/Definitions/IdentityServer/IdentityServerConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer;
+
+namespace Jevstafjev.Anecdotes.AuthServer.Web.Definitions.IdentityServer;
+
+public class IdentityServerConfiguration
+{
+    public static IEnumerable<Client> GetClients()
+    {
+        yield return new Client
+        {
+            ClientId = "blazor-client",
+            AllowedGrantTypes = GrantTypes.Code,
+            RequireClientSecret = false,
+            RequireConsent = false,
+            RequirePkce = true,
+            AllowedScopes =
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Address,
+                IdentityServerConstants.StandardScopes.Profile,
+                IdentityServerConstants.StandardScopes.Email,
+                "AnecdoteApi"
+            },
+            RedirectUris =
+            {
+                "https://localhost:10001/swagger/oauth2-redirect.html",
+                "https://localhost:5001/authentication/login-callback"
+            },
+            PostLogoutRedirectUris = { "https://localhost:5001/authentication/logout-callback" },
+            AllowedCorsOrigins = { "https://localhost:5001" }
+        };
+    }
+
+    public static IEnumerable<ApiResource> GetApiResources()
+    {
+        yield return new ApiResource("AnecdoteApi");
+    }
+
+    public static IEnumerable<IdentityResource> GetIdentityResources()
+    {
+        yield return new IdentityResources.OpenId();
+        yield return new IdentityResources.Address();
+        yield return new IdentityResources.Profile();
+        yield return new IdentityResources.Email();
+    }
+
+    public static IEnumerable<ApiScope> GetScopes()
+    {
+        yield return new ApiScope("AnecdoteApi");
+    }
+}
---
+++ src/Jevstafjev.Anecdotes.AuthServer/Jevstafjev.Anecdotes.AuthServer.Web/Definitions/IdentityServer/IdentityServerDefinition.cs
@@ -0,0 +1,38 @@
+﻿using Jevstafjev.Anecdotes.AuthServer.Infrastructure;
+using Jevstafjev.Anecdotes.AuthServer.Web.Application.Services;
+using Jevstafjev.Anecdotes.AuthServer.Web.Definitions.Base;
+using Microsoft.AspNetCore.Identity;
+
+namespace Jevstafjev.Anecdotes.AuthServer.Web.Definitions.IdentityServer;
+
+public class IdentityServerDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireUppercase = false;
+        })
+            .AddSignInManager<SignInManager<ApplicationUser>>()
+            .AddUserManager<UserManager<ApplicationUser>>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        builder.Services.AddIdentityServer()
+            .AddAspNetIdentity<ApplicationUser>()
+            .AddInMemoryClients(IdentityServerConfiguration.GetClients())
+            .AddInMemoryApiResources(IdentityServerConfiguration.GetApiResources())
+            .AddInMemoryIdentityResources(IdentityServerConfiguration.GetIdentityResources())
+            .AddInMemoryApiScopes(IdentityServerConfiguration.GetScopes())
+            .AddProfileService<ProfileService>()
+            .AddDeveloperSigningCredential();
+    }
+
+    public override void ConfigureApplication(WebApplication app)
+    {
+        app.UseIdentityServer();
+    }
+}
