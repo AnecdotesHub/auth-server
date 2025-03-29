5fd8a23106d447a00f13ce41f403c913665a73eb
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 16:03:57 +0200

    Create CommonDefinition.cs

---
+++ src/Jevstafjev.Anecdotes.AuthServer/Jevstafjev.Anecdotes.AuthServer.Web/Definitions/Common/CommonDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Jevstafjev.Anecdotes.AuthServer.Web.Definitions.Base;
+
+namespace Jevstafjev.Anecdotes.AuthServer.Web.Definitions.Common;
+
+public class CommonDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+    }
+
+    public override void ConfigureApplication(WebApplication app)
+    {
+        app.UseHttpsRedirection();
+        app.MapRazorPages();
+    }
+}
