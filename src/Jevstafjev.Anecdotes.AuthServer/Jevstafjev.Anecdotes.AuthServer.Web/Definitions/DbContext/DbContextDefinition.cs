776a406e6848454990a136747d3bd1e617ff928e
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 13:56:42 +0200

    Add Database connection

---
+++ src/Jevstafjev.Anecdotes.AuthServer/Jevstafjev.Anecdotes.AuthServer.Web/Definitions/DbContext/DbContextDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Jevstafjev.Anecdotes.AuthServer.Infrastructure;
+using Jevstafjev.Anecdotes.AuthServer.Web.Definitions.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jevstafjev.Anecdotes.AuthServer.Web.Definitions.DbContext;
+
+public class DbContextDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            var connectionString = builder.Configuration.GetConnectionString(nameof(ApplicationDbContext));
+            options.UseSqlServer(connectionString);
+        });
+    }
+}
