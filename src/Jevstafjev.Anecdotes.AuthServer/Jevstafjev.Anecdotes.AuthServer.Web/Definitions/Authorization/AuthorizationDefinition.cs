e368e117a3a6437ec8fe104f51c68ceb3ff740af
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 16:56:53 +0200

    Create AuthorizationDefinition.cs

---
+++ src/Jevstafjev.Anecdotes.AuthServer/Jevstafjev.Anecdotes.AuthServer.Web/Definitions/Authorization/AuthorizationDefinition.cs
@@ -0,0 +1,18 @@
+﻿using Jevstafjev.Anecdotes.AuthServer.Web.Definitions.Base;
+
+namespace Jevstafjev.Anecdotes.AuthServer.Web.Definitions.Authorization;
+
+public class AuthorizationDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication();
+        builder.Services.AddAuthorization();
+    }
+
+    public override void ConfigureApplication(WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+    }
+}
