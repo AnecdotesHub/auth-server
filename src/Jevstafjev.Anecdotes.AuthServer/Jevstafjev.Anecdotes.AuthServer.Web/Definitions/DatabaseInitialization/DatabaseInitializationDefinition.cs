5063f1d265d1cd9a4504b4041a831ddc28d4bf12
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 16:45:56 +0200

    Add database initialization

---
+++ src/Jevstafjev.Anecdotes.AuthServer/Jevstafjev.Anecdotes.AuthServer.Web/Definitions/DatabaseInitialization/DatabaseInitializationDefinition.cs
@@ -0,0 +1,12 @@
+﻿using Jevstafjev.Anecdotes.AuthServer.Infrastructure.DatabaseInitialization;
+using Jevstafjev.Anecdotes.AuthServer.Web.Definitions.Base;
+
+namespace Jevstafjev.Anecdotes.AuthServer.Web.Definitions.DatabaseInitialization;
+
+public class DatabaseInitializationDefinition : AppDefinition
+{
+    public override void ConfigureApplication(WebApplication app)
+    {
+        DatabaseInitializer.SeedUsers(app.Services).Wait();
+    }
+}
