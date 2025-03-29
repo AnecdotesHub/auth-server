e056c1a2ac14de484769c49df07f56c260902bb8
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 13:19:03 +0200

    Add DbContext

---
+++ src/Jevstafjev.Anecdotes.AuthServer/Jevstafjev.Anecdotes.AuthServer.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Jevstafjev.Anecdotes.AuthServer.Infrastructure.Base;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jevstafjev.Anecdotes.AuthServer.Infrastructure;
+
+public class ApplicationDbContext : DbContextBase
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+}
---
+++ src/Jevstafjev.Anecdotes.AuthServer/Jevstafjev.Anecdotes.AuthServer.Infrastructure/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Jevstafjev.Anecdotes.AuthServer.Infrastructure
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+    }
+}
